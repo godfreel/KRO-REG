c9686228cfa87e8e60a0b5d65eca6dc6cc86b0dc
dzmitryDubrovin <dzmitry_dubrovin@dancosoft.com>
2014-01-14 23:17:46 +0300

    adding config module

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+var express = require('express');
 var http = require('http');
+var path = require('path');
 var url = require('url');
+
+
+var app = express();
+
+app.engine('ejs', require('ejs-locals'));
+app.set('views', __dirname + '/template');
+app.set('view engine', 'ejs');
 
 http.createServer(function(req, res) {
     
