898ead6039b53d739175a66a5102e080ec549ca7
dzmitryDubrovin <dzmitry_dubrovin@dancosoft.com>
2014-04-14 19:27:31 +0300

    update express to 4.0.0 version

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var log = require('./lib/log')(module);
+
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var favicon        = require('static-favicon');
+var cookieParser   = require('cookie-parser');
+
 var config = require('./config');
 var mongoose = require('./lib/mongoose');
 var HttpError = require('./error').HttpError;
-var MongoStore = require('connect-mongo')(express);
+// var MongoStore = require('connect-mongo')(express);
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.methodOverride());
+app.use(favicon());
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(methodOverride());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(app.router);
 require('./routes')(app);
 
 app.use(require('./middleware/sendHttpError'));
