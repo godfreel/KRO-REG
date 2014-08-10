629e11581e0e2666ce7e77d987c474d0b7196ce9
dzmitryDubrovin <dzmitry_dubrovin@dancosoft.com>
2014-08-10 23:08:21 +0300

    remove quintus

--- TEST/quintus/pac-man/scripts/scenes.js
+++
@@ -1,8 +0,0 @@
-Game.Scenes = (function(){
-	Q.scene("level1",function(stage) {
-	  var map = stage.collisionLayer(new Q.TowerManMap());
-	  map.setup();
-
-	  var player = stage.insert(new Q.Player(Q.tilePos(10,7)));
-	});
-});
